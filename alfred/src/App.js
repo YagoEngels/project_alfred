ecefbb85562d6ea1f18548d9c8dfae916f65bad7
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2023-03-31 01:08:58 +0200

    commit 1

---
+++ alfred/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input type="text" name="uname" required></input>
+    
+        <input type="password" name="psw" required></input>
+      </header>
+    </div>
+  );
+}
+
+export default App;
