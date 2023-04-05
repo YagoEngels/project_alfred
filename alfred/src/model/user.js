c96a1c746a3131d35bc8045bf734a600479cbb66
PC001 laptop <yago.engels@gmail.com>
2023-04-05 07:36:25 +0200

    users class is added

---
+++ alfred/src/model/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(name,secondName,code,email,tel,password){
+        this.name = name;
+        this.secondName = secondName;
+        this.code = code;
+        this.email = email;
+        this.tel = tel;
+        this.password = password;
+    }
+}
